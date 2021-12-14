132f23e51eb75d971c06a233bfdd8059b8a28446
Suryansh-23 <suryanshchauhan2005@gmail.com>
2021-12-14 18:15:53 +0530

    Changes in python api & server.js deleted

--- python/Vault.py
+++ python/Vault.py
@@ -1,11 +1,13 @@
+from json import dumps
 import json
-import os
-import pickle
-import shutil
+from os.path import splitext
+from pickle import load
 from mimetypes import guess_type as mime_type
 from urllib.parse import unquote
+import random as rd
+import string
 
-import flask
+from flask import Flask
 from flask import jsonify, request
 
 type_map_icon = {
@@ -18,6 +20,12 @@ type_map_icon = {
 
 key_map_file = {}
 locked = True
+AUTH = False
+
+
+def random_key():
+    str_pool = string.ascii_letters + string.digits
+    return rd.choices(str_pool, k=20)
 
 
 def manipulate(dic, sep=""):
@@ -28,7 +36,7 @@ def manipulate(dic, sep=""):
         if str.isdigit(str(i)):
             temp["label"] = dic[i]
             key_map_file[temp["key"]] = temp["label"]
-            temp["ext"] = os.path.splitext(temp["label"])[-1][1:]
+            temp["ext"] = splitext(temp["label"])[-1][1:]
             mime = mime_type(temp["label"])[0]
             if mime:
                 file_type = mime.split("/")[0]
@@ -44,10 +52,10 @@ def manipulate(dic, sep=""):
 
 
 with open(".\.\Vault.pickle", "rb") as fobj:
-    dic = pickle.load(fobj)
+    dic = load(fobj)
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 dir_structure = manipulate(dic["__Tree__"])
 
 
@@ -56,30 +64,39 @@ def home():
     return "RESTFul API"
 
 
-@app.route("/api/v1/pswrd", methods=["GET"])
+@app.route("/api/v1/auth")
 def password():
-    return jsonify(dic["__Pswrd__"])
+    if dic["__Pswrd__"] == request.headers.get("Pass-Key"):
+        key = random_key()
+        global AUTH
+        AUTH = key
+        return jsonify(key)
+    return jsonify(False), 401
 
 
 @app.route("/api/v1/tree", methods=["GET"])
 def tree():
-    return json.dumps(dir_structure)
+    if AUTH == request.headers.get("Auth-Key"):
+        return jsonify(dumps(dir_structure))
+    return jsonify(False), 401
 
 
 @app.route("/api/v1/save", methods=["GET"])
 def save():
-    try:
-        key = request.args["key"]
-        path = unquote(request.args["path"])
-        for i in key_map_file.keys():
-            if i == key:
-                file_name = key_map_file[i]
-                break
-        with open(path, "wb") as fobj:
-            fobj.write(dic[file_name])
-        return jsonify(True)
-    except:
-        return jsonify(False)
+    if AUTH == request.headers.get("Auth-Key"):
+        try:
+            key = request.header.get("Key")
+            path = unquote(request.header.get("Path"))
+            for i in key_map_file.keys():
+                if i == key:
+                    file_name = key_map_file[i]
+                    break
+            with open(path, "wb") as fobj:
+                fobj.write(dic[file_name])
+            return jsonify(True)
+        except:
+            return jsonify(False)
+    return jsonify(False), 401
 
 
 app.run(host="127.7.3.0", port=1728)
